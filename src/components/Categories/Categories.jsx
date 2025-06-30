70a3b657f3c98c59a159ce7928376a4dd290b75f
Pavel Samar <Spa15@mail.ru>
2025-06-30 16:13:18 +0300

    refactoring

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -8,6 +8,13 @@ export default function Categories({
 }) {
 	return (
 		<div className={styles.categories}>
+			<button
+				onClick={() => setSelectedCategory(null)}
+				className={!selectedCategory ? styles.active : styles.item}
+			>
+				All
+			</button>
+			
 			{categories.map((category) => {
 				return (
 					<button
--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import styles from "./styles.module.css";
 import { formatTimeAgo } from "../../helpers/formatTimeAgo";
 import Image from "../Image/Image";
+import withSkeleton from "../../helpers/hocs/withSkeleton";
 
-export default function NewsBanner({ item }) {
+const NewsBanner = ({ item }) => {
 	return (
 		<header className={styles.banner}>
 			<Image image={item.image} />
@@ -13,4 +14,8 @@ export default function NewsBanner({ item }) {
 			</p>
 		</header>
 	);
-}
+};
+
+const NewsBannerWithSkeleton = withSkeleton(NewsBanner, "banner", 1);
+
+export default NewsBannerWithSkeleton;
